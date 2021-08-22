481f5b8eab720175062ec18383661658a1b760a7
luoyingmm <1502972236@qq.com>
2021-08-22 17:16:18 +0800

    Login complete

--- app/src/main/java/com/luoyingmm/leandemo/MainActivity.kt
+++ app/src/main/java/com/luoyingmm/leandemo/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.luoyingmm.leandemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menulogout){
+            startActivity(Intent(this,LoginActivity::class.java))
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
